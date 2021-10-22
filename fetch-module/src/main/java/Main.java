35a5857d275059b514f6ec7fc30deac7b5a44b38
Bartosz Starosta <bstarosta77@gmail.com>
2021-10-22 19:55:23 +0200

    Added ravelry api

--- fetch-module/src/main/java/Main.java
+++ fetch-module/src/main/java/Main.java
@@ -1,19 +1,16 @@
-import akka.actor.ActorSystem;
-import akka.japi.function.Procedure;
-import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Keep;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
-import lombok.val;
+import okhttp3.ResponseBody;
+import raverlyapi.RavelryApi;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        val system = ActorSystem.create();
-        val source = Source.range(1, 5);
-        val flow = Flow.of(Integer.class).map(Object::toString);
-        val sink = Sink.foreach((Procedure<String>) System.out::println);
-        val graph = source.via(flow).toMat(sink, Keep.right());
-        val completionStage = graph.run(system);
-        completionStage.whenComplete((Object d, Object t) -> system.terminate());
+        try {
+            ResponseBody res = RavelryApi.getRavelryData("patterns/patterns.json");
+            System.out.print(res.string());
+        }
+        catch (IOException ex) {
+            System.out.println("error");
+        }
     }
 }
---
+++ fetch-module/src/main/java/esflow/GetIDsFlow.java
@@ -0,0 +1,5 @@
+package esflow;
+
+
+class GetIDsFlow {
+}
