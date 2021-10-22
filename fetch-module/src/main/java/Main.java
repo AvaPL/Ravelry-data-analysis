d125cb2122b5f22f744ad9fc63fbbcf5d856a495
Bartosz Starosta <bstarosta77@gmail.com>
2021-10-22 21:42:19 +0200

    Added GetIDsFlow

--- fetch-module/src/main/java/Main.java
+++ fetch-module/src/main/java/Main.java
@@ -1,16 +1,7 @@
-import okhttp3.ResponseBody;
-import raverlyapi.RavelryApi;
 
-import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ResponseBody res = RavelryApi.getRavelryData("patterns/patterns.json");
-            System.out.print(res.string());
-        }
-        catch (IOException ex) {
-            System.out.println("error");
-        }
+
     }
 }
