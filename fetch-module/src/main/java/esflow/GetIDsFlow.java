e1c8606b32f8feb073370bddcc504a04cdd94b0b
Bartosz Starosta <bstarosta77@gmail.com>
2021-10-23 18:55:58 +0200

    Made RavelryApi calls asynchronous

--- fetch-module/src/main/java/esflow/GetIDsFlow.java
+++ fetch-module/src/main/java/esflow/GetIDsFlow.java
@@ -2,6 +2,7 @@ package esflow;
 
 
 import okhttp3.ResponseBody;
+import pojo.PageInfo;
 import raverlyapi.RavelryApi;
 
 import java.io.IOException;
@@ -12,17 +13,17 @@ import java.util.Map;
 public class GetIDsFlow {
 
     //TODO Needs refactoring
-    public static ResponseBody getIDs(String url, Integer pageNumber, Integer pageLimit) {
-        Map<String, String> params = new HashMap<>();
-        params.put("page", pageNumber.toString());
-        params.put("page_size", pageLimit.toString());
-        ResponseBody responseBody = null;
-        try {
-           responseBody = RavelryApi.getRavelryData(url, params);
-        } catch (IOException exception) {
-            exception.printStackTrace();
-        }
-
-        return responseBody;
-    }
+//    public static ResponseBody getIDs(String url, PageInfo pageInfo) {
+//        Map<String, String> params = new HashMap<>();
+//        params.put("page", pageNumber.toString());
+//        params.put("page_size", pageLimit.toString());
+//        ResponseBody responseBody = null;
+//        try {
+//           responseBody = RavelryApi.getRavelryData(url, params);
+//        } catch (IOException exception) {
+//            exception.printStackTrace();
+//        }
+//
+//        return responseBody;
+//    }
 }
