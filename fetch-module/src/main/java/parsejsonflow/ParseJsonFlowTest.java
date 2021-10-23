ae4a489f9e7e14fc676c87d412358635a881117f
agngan <agnieszka.gano@gmail.com>
2021-10-23 21:43:12 +0200

    Added function for parsing a patterns json node to pattern ids.

--- fetch-module/src/main/java/parsejsonflow/ParseJsonFlowTest.java
+++ fetch-module/src/main/java/parsejsonflow/ParseJsonFlowTest.java
@@ -13,13 +13,15 @@ import lombok.val;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ParseJsonFlowTest {
 
     // Function for parsing a single json node to an iterable of json nodes. To be used later in main.
-    public static Function<JsonNode, Iterable<JsonNode>> parseJsonNodes() {
+    // TODO: delete static and sout
+    public static Function<JsonNode, Iterable<JsonNode>> parseJsonNodeToJsonNodes(String nodesName) {
         return jsonNode -> {
-            ArrayNode arrayNode = (ArrayNode) jsonNode.get("patterns");
+            ArrayNode arrayNode = (ArrayNode) jsonNode.get(nodesName);
             List<JsonNode> jsonNodeList = new ArrayList<>();
             arrayNode.forEach(jsonNodeList::add);
             System.out.println(jsonNodeList);
@@ -27,7 +29,18 @@ public class ParseJsonFlowTest {
         };
     }
 
-    // Main for testing the aforementioned parsing function.
+    // Function for parsing a patterns json node to an iterable of pattern ids.
+    // TODO: delete static and sout
+    public static Function<JsonNode, Iterable<Integer>> parseJsonNodeToPatternIds(String nodesName) {
+        return jsonNode -> {
+            List<JsonNode> jsonNodeList = (List<JsonNode>) parseJsonNodeToJsonNodes(nodesName).apply(jsonNode);
+            List<Integer> patternIds = jsonNodeList.stream().map(jsonNode1 -> jsonNode1.get("id").asInt()).collect(Collectors.toList());
+            System.out.println(patternIds);
+            return patternIds;
+        };
+    }
+
+    // Main for testing parsing functions.
     public static void main(String[] args) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode jsonNodeExample = mapper.readTree("""
@@ -35,8 +48,18 @@ public class ParseJsonFlowTest {
                     "patterns": [
                         {
                             "free": false,
-                            "id": 1168670,
+                            "id": 1,
                             "name": "Shawlography: Westknits MKAL 2021",
+                            "first_photo": {
+                            "id": 106535972,
+                            "sort_order": 1,
+                            "x_offset": 0
+                            }
+                        },
+                        {
+                            "free": false,
+                            "id": 2,
+                            "name": "Slipstravaganza",
                             "first_photo": {
                             "id": 106535972,
                             "sort_order": 1,
@@ -46,7 +69,7 @@ public class ParseJsonFlowTest {
                     ]
                 }""");
 
-        val flow = Flow.of(JsonNode.class).map(parseJsonNodes());
+        val flow = Flow.of(JsonNode.class).map(parseJsonNodeToPatternIds("patterns"));
         val system = ActorSystem.create();
         Source.single(jsonNodeExample).via(flow).to(Sink.ignore()).run(system);
     }
