c89e5bf0df95b5f91e6996555d4f5464f7692695
agngan <agnieszka.gano@gmail.com>
2021-10-24 19:49:20 +0200

    Added function for getting yarn ids from json nodes.

--- fetch-module/src/main/java/parsejsonflow/ParseJsonFlowTest.java
+++ fetch-module/src/main/java/parsejsonflow/ParseJsonFlowTest.java
@@ -1,27 +1,20 @@
 package parsejsonflow;
 
-import akka.actor.ActorSystem;
 import akka.japi.function.Function;
-import akka.stream.javadsl.Flow;
-import akka.stream.javadsl.Sink;
-import akka.stream.javadsl.Source;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
-import lombok.val;
 
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class ParseJsonFlowTest {
 
     // Function for parsing a single json node to an iterable of json nodes. To be used later in main.
     // TODO: delete static and sout
-    public static Function<JsonNode, Iterable<JsonNode>> parseJsonNodeToJsonNodes(String nodesName) {
+    public static Function<JsonNode, Iterable<JsonNode>> parseJsonNodeToJsonNodes(String nodesListName) {
         return jsonNode -> {
-            Iterator<JsonNode> jsonNodesIterator = jsonNode.get(nodesName).elements();
+            Iterator<JsonNode> jsonNodesIterator = jsonNode.get(nodesListName).elements();
             List<JsonNode> jsonNodeList = Lists.newArrayList(jsonNodesIterator);
             System.out.println(jsonNodeList);
             return jsonNodeList;
@@ -30,17 +23,31 @@ public class ParseJsonFlowTest {
 
     // Function for parsing a patterns json node to an iterable of pattern ids.
     // TODO: delete static and sout
-    public static Function<JsonNode, Iterable<Integer>> parseJsonNodeToPatternIds(String nodesName) {
+    public static Function<JsonNode, Iterable<Integer>> parseJsonNodeToIds(String nodesListName) {
         return jsonNode -> {
-            List<JsonNode> jsonNodeList = (List<JsonNode>) parseJsonNodeToJsonNodes(nodesName).apply(jsonNode);
-            List<Integer> patternIds = jsonNodeList.stream().map(jsonNode1 -> jsonNode1.get("id").asInt()).collect(Collectors.toList());
-            System.out.println(patternIds);
-            return patternIds;
+            List<JsonNode> jsonNodeList = (List<JsonNode>) parseJsonNodeToJsonNodes(nodesListName).apply(jsonNode);
+            List<Integer> ids = jsonNodeList.stream().map(jsonNode1 -> jsonNode1.get("id").asInt()).collect(Collectors.toList());
+            System.out.println(ids);
+            return ids;
+        };
+    }
+
+    // Function for parsing a patterns json node to an iterable of pattern ids.
+    // TODO: delete static and sout
+    public static Function<Iterable<JsonNode>, Iterable<Integer>> parseJsonNodesToYarnIds() {
+        return jsonNodes -> {
+            Set<Integer> yarnIds = new HashSet<>();
+            for (JsonNode jsonNode : jsonNodes) {
+                List<JsonNode> yarns = (List<JsonNode>) parseJsonNodeToJsonNodes("packs").apply(jsonNode);
+                yarns.forEach(jsonNode1 -> yarnIds.add(jsonNode1.get("yarn_id").asInt()));
+            }
+            System.out.println(yarnIds);
+            return yarnIds;
         };
     }
 
     // Main for testing parsing functions.
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode searchExample = mapper.readTree("""
                 {
@@ -94,8 +101,26 @@ public class ParseJsonFlowTest {
                     }
                 }""");
 
-        val flow = Flow.of(JsonNode.class).map(parseJsonNodeToPatternIds("patterns"));
-        val system = ActorSystem.create();
-        Source.single(searchExample).via(flow).to(Sink.ignore()).run(system);
+        JsonNode yarnIdsExample = mapper.readTree("""
+                {
+                    "comments_count": 37,
+                    "created_at": "2021/09/01 10:00:26 -0400",
+                    "packs": [
+                        {
+                        "id": 101189144,
+                        "yarn_id": 27830,
+                        "yarn_name": "Isager Yarn Isager Alpaca 2"
+                        },
+                        {
+                        "id": 101189138,
+                        "yarn_id": 46174,
+                        "yarn_name": "Holst Garn Supersoft 100% Uld"
+                        }
+                    ]
+                }""");
+
+        parseJsonNodeToIds("patterns").apply(searchExample);
+        parseJsonNodeToJsonNodes("patterns").apply(patternsByIdsExample);
+        parseJsonNodesToYarnIds().apply(Collections.singletonList(yarnIdsExample));
     }
 }
