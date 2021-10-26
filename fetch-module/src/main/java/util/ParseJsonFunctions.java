9356a5e2ca3e0d99fe510fbcadd73f2584f035f6
Paweł Cembaluk <241574@student.pwr.edu.pl>
2021-10-26 22:06:39 +0200

    Added null check to ParseJsonFunctions.

--- fetch-module/src/main/java/util/ParseJsonFunctions.java
+++ fetch-module/src/main/java/util/ParseJsonFunctions.java
@@ -3,10 +3,12 @@ package util;
 import akka.japi.function.Function;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Lists;
+import lombok.val;
 
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class ParseJsonFunctions {
@@ -14,7 +16,10 @@ public class ParseJsonFunctions {
     // Function for parsing a single json node to an iterable of json nodes. To be used later in main.
     public static Function<JsonNode, Collection<JsonNode>> parseJsonNodeToJsonNodes(String nodesListName) {
         return jsonNode -> {
-            Iterator<JsonNode> jsonNodesIterator = jsonNode.get(nodesListName).elements();
+            val nodesList = jsonNode.get(nodesListName);
+            if (nodesList == null)
+                return List.of();
+            Iterator<JsonNode> jsonNodesIterator = nodesList.elements();
             return Lists.newArrayList(jsonNodesIterator);
         };
     }
@@ -22,8 +27,12 @@ public class ParseJsonFunctions {
     // Function for parsing a patterns json node to an iterable of pattern ids.
     public static Function<JsonNode, Collection<Integer>> parseJsonNodeToIds(String nodesListName) {
         return jsonNode -> {
-            List<JsonNode> jsonNodeList = (List<JsonNode>) parseJsonNodeToJsonNodes(nodesListName).apply(jsonNode);
-            return jsonNodeList.stream().map(jsonNode1 -> jsonNode1.get("id").asInt()).collect(Collectors.toList());
+            val jsonNodeList = parseJsonNodeToJsonNodes(nodesListName).apply(jsonNode);
+            return jsonNodeList.stream()
+                    .map(entity -> entity.get("id"))
+                    .filter(Objects::nonNull)
+                    .map(JsonNode::asInt)
+                    .collect(Collectors.toList());
         };
     }
 }
